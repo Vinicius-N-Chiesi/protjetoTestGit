a1818cbbae995d9df1bf4945b86bf73d25dc0b39
Vinicius Chiesi <mvcryptobr@protonmail.com>
2022-04-09 05:52:25 -0300

    Versao 1.0

---
+++ ConversorFahrenheit.py
@@ -0,0 +1,10 @@
+#Program to convert temperature en celsius to fahrenheit
+#Program para converte celsius para fahrenheit
+
+
+#Change this value for a diferent result
+#Insira o valores para doferentes resultados
+
+celsius = float(input("Digite a temperatura em celsius!"))
+fahrenheit = float((celsius * 1.8) + 32)
+print("A temperatura é ", fahrenheit )
