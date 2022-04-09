17d7934d2be28ddc82450ace02cee723a1c6f108
Vinicius Chiesi <mvcryptobr@protonmail.com>
2022-04-09 05:51:24 -0300

    Commit Inicial

---
+++ teste1C.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	int number;
+	
+	cout << "Coloque um numero:";
+	cin >> number;
+	
+	cout << "Seu numero e" << number;
+	
+	return 0;
+}
