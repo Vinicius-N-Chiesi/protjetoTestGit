a8c9adffd14c31e3aa6360a968f20db9378d2e65
Vinicius Chiesi <mvcryptobr@protonmail.com>
2022-04-09 06:27:48 -0300

    Programa para transpor matrizes

---
+++ TransporMatriz.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	int a[10][10], transpose[10][10], row, colum, i, j;
+	
+	cout << "Entre com as linhas e colunas";
+	cin >> row >> colum;
+}
