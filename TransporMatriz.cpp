4fabd2b2efad148294ccec9be1d3b8a8defbb3ea
Vinicius Chiesi <mvcryptobr@protonmail.com>
2022-04-10 21:41:43 -0300

    Versao 1.1 - Programa monta a matriz

--- TransporMatriz.cpp
+++ TransporMatriz.cpp
@@ -3,8 +3,22 @@ using namespace std;
 
 int main(){
 	
-	int a[10][10], transpose[10][10], row, colum, i, j;
+	int a[10][10], transpose[10][10], row, column, i, j;
 	
-	cout << "Entre com as linhas e colunas";
-	cin >> row >> colum;
+	cout << "Entre com as linhas e colunas"; //Linha para captar o numero de linha e colunas
+	cin >> row >> column;
+	
+	cout << "\nEntrando com elementos da matriz" << endl;
+	
+	//Criando a matriz
+	
+	for (int i = 0; i < row; ++i){
+		for(int j = 0; j < column; ++j){
+			cout << "Entre com o elemento" << i + 1 << j + 1 << ": ";
+			cin >> a[10][10];
+		}
+	}
+	
+	
+	
 }
