ba60ad82e5886f5f52f303af299d7c859127d85b
Vinicius Chiesi <mvcryptobr@protonmail.com>
2022-04-11 03:15:06 -0300

    Versao 1.2 - + Escrevendo Matriz e ajustes

--- TransporMatriz.cpp
+++ TransporMatriz.cpp
@@ -5,7 +5,7 @@ int main(){
 	
 	int a[10][10], transpose[10][10], row, column, i, j;
 	
-	cout << "Entre com as linhas e colunas"; //Linha para captar o numero de linha e colunas
+	cout << "Entre com as linhas e colunas: \n"; //Linha para captar o numero de linha e colunas
 	cin >> row >> column;
 	
 	cout << "\nEntrando com elementos da matriz" << endl;
@@ -15,10 +15,19 @@ int main(){
 	for (int i = 0; i < row; ++i){
 		for(int j = 0; j < column; ++j){
 			cout << "Entre com o elemento" << i + 1 << j + 1 << ": ";
-			cin >> a[10][10];
+			cin >> a[i][j];
 		}
 	}
 	
+	// Escrevendo a Matriz
+	for (int i = 0; i < row; ++i){
+		for (int j = 0; j < column; ++j){
+			cout << " " << a[i][j];
+			if(j == column - 1){
+				cout << endl << endl;
+			}
+		}
+	}
 	
 	
 }
